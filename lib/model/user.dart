50b1b3e9f0fba437d1526474c52a91d3c8a687e6
Seiyeon Joo <dorothy01427@naver.com>
2024-05-03 15:38:26 +0900

    feat: finish login ui

---
+++ lib/model/user.dart
@@ -0,0 +1,11 @@
+//이름, 아이디, 비밀번호, 국적
+
+class User {
+  final String name;
+  final String id;
+  final String password;
+  final String country;
+
+  User({required this.name, required this.id, required this.password,required this.country});
+}
+
--- lib/view/front/loginPage.dart
+++ lib/view/front/loginPage.dart
@@ -1,32 +1,50 @@
-//email, password
-
-import 'package:mobileplatform_project/view/front/signupPage.dart';
-import 'package:mobileplatform_project/view/front/middlePage.dart';
-import 'package:mobileplatform_project/view/home/homePage.dart';
 import 'package:flutter/material.dart';
+import 'package:mobileplatform_project/view/front/signupPage.dart';
+import 'package:mobileplatform_project/view/home/homePage.dart';
 
 void main() {
   runApp(const LoginPage());
 }
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icons/logo.jpeg', // 이미지 파일 경로 설정
+                  width: 50, // 이미지 너비 설정
+                  height: 50, // 이미지 높이 설정
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'DAKUA',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
             TextField(
               // controller: _emailController,
               decoration: InputDecoration(
-                labelText: 'Email',
+                labelText: 'ID',
+                filled: true, // 배경색 채우기
+                fillColor: Colors.grey[200], // 배경색 변경
+                border: OutlineInputBorder( // 둥근 테두리
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
               ),
             ),
             SizedBox(height: 20),
@@ -35,19 +53,43 @@ class LoginPage extends StatelessWidget {
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
+                filled: true, // 배경색 채우기
+                fillColor: Colors.grey[200], // 배경색 변경
+                border: OutlineInputBorder( // 둥근 테두리
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity, // 버튼 너비를 화면 가로 전체로 확장
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green, // 버튼 색상 변경
+                  padding: EdgeInsets.symmetric(vertical: 15.0), // 버튼 내부 패딩
+                ),
+                onPressed: () => _navigateToMainPage(context),
+                child: Text(
+                  '로그인하기',
+                  style: TextStyle(fontSize: 18.0), // 폰트 크기 변경
+                ),
               ),
             ),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  child: Text("Login"),
-                  onPressed: () => _navigateToMainPage(context),
+                Text(
+                  '아직 회원이 아니라면?',
+                  style: TextStyle(fontSize: 14.0), // 폰트 크기 변경
                 ),
-                ElevatedButton(
-                  child: Text("Sign up"),
+                SizedBox(width: 5),
+                TextButton(
                   onPressed: () => _navigateToSignUpPage(context),
+                  child: Text(
+                    '회원가입',
+                    style: TextStyle(fontSize: 14.0), // 폰트 크기 변경
+                  ),
                 ),
               ],
             ),
