c0c639096c3abc9cc8ceeae462da497f1a587a16
Seiyeon Joo <dorothy01427@naver.com>
2024-05-30 16:43:19 +0900

    add summary in historyPage

--- lib/view/history/historySummaryPage.dart
+++ lib/view/history/historySummaryPage.dart
@@ -5,15 +5,21 @@ import '../../model/user.dart';
 import '../../viewModel/aiProcess_viewModel.dart';
 import '../../dataSource/aiProcess_dataSource.dart';
 
-class HistorySummaryPage extends StatelessWidget {
+class HistorySummaryPage extends StatefulWidget {
   final User user;
   final Map<String, String?> data;
 
   const HistorySummaryPage({Key? key, required this.user, required this.data}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  _HistorySummaryPageState createState() => _HistorySummaryPageState();
+}
 
+class _HistorySummaryPageState extends State<HistorySummaryPage> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
     return ChangeNotifierProvider<AIProcessViewModel>(
       create: (context) => AIProcessViewModel(AIProcessDataSource()),
       child: Scaffold(
@@ -30,9 +36,9 @@ class HistorySummaryPage extends StatelessWidget {
                     width: 700,
                     height: 350,
                     color: Colors.grey[300],
-                    child: data['image'] != null
+                    child: widget.data['image'] != null
                         ? Image.memory(
-                      base64.decode(data['image']!),
+                      base64.decode(widget.data['image']!),
                       fit: BoxFit.cover,
                     )
                         : Container(),
@@ -41,13 +47,39 @@ class HistorySummaryPage extends StatelessWidget {
                   Container(
                     width: double.infinity,
                     padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                    color: Colors.grey[200],
-                    child: Text(
-                      data['text_data'] != null
-                          ? data['text_data']!
-                          : '요약된 내용이 없습니다.',
-                      style: TextStyle(fontSize: 16.0),
-                      textAlign: TextAlign.center,
+                    child: ExpansionPanelList(
+                      expandedHeaderPadding: EdgeInsets.zero,
+                      expansionCallback: (int index, bool isExpanded) {
+                        setState(() {
+                          _isExpanded = !_isExpanded;
+                        });
+                      },
+                      children: [
+                        ExpansionPanel(
+                          headerBuilder: (BuildContext context, bool isExpanded) {
+                            return ListTile(
+                              title: Text(
+                                widget.data['text_data'] != null
+                                    ? widget.data['text_data']!
+                                    : '요약된 내용이 없습니다.',
+                                style: TextStyle(fontSize: 16.0),
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          },
+                          body: Container(
+                              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                            child: Text(
+                              widget.data['full_text_data'] != null
+                                  ? widget.data['full_text_data']!
+                                  : '전체 내용이 없습니다.',
+                              style: TextStyle(fontSize: 16.0),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          isExpanded: _isExpanded,
+                        ),
+                      ],
                     ),
                   ),
                 ],
